a57bebebbf4491ea607167426a82008a720b640d
BoogeyMan <boogeyman1998@gmail.com>
2019-09-01 16:08:57 +0700

    Fix module Cat & Mouse

--- Cat.js
+++ Cat.js
@@ -2,3 +2,6 @@ function Cat(name, color) {
 	this.name = name,
 	this.color = color
 }
+Cat.prototype.eat = {
+	console.log('Eatingg');
+} 
--- Mouse.js
+++ Mouse.js
@@ -2,3 +2,7 @@ function Mouse(name, weight) {
 	this.name = name,
 	this.weight = weight
 }
+
+Mouse.prototype.eat = {
+	console.log('ăn');
+}
